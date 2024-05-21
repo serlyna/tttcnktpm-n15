178e398f9e370f58e968c2213c738252fdd9a2eb
Khanhthi115 <khanhthi115@gmail.com>
2024-05-21 22:18:01 +0700

    xoa file du

--- templates/back/doc/form-delete-product.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $kn = mysqli_connect('localhost','root','','book');
-    $id = $_GET['id_product'];
-    $sql = "delete from product where id_product=".$_GET['id_product'];
-    $kq = mysqli_query($kn, $sql) or die("Lỗi truy vấn");
-    header("location:table-data-product.php");
-?>
